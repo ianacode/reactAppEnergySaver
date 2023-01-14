3d7616274c54705ca20f11aee0ad6fa0d3c41d33
Simone Federici <s.federici@geniusbytes.it>
2023-01-15 00:30:53 +0100

    hover mouse/touch

--- src/components/Footer/components/Menu.jsx
+++ src/components/Footer/components/Menu.jsx
@@ -5,10 +5,15 @@ const Menu = (props) => {
   const navigate = useNavigate();
   const resolved = useResolvedPath(props.link);
   const match = useMatch({ path: resolved.pathname, end: true });
+  const [hover, setHover] = React.useState('');
 
   return (
-    <div className={`menu ${props.className} ${match ? 'active' : ''}`}
-          onClick={()=>navigate(props.link)}>
+    <div className={`menu ${props.className} ${match ? 'active' : ''} ${hover}`}
+         onClick={() => navigate(props.link)}
+         onMouseEnter={() => setHover('active')}
+         onMouseLeave={() => setHover('')}
+         onTouchStart={() => setHover('active')}
+         onTouchEnd={() => setHover('')}>
       <img className="imageMenu" src={props.svg} />
       <span>{props.label}</span>
     </div>
