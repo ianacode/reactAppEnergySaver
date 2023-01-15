60394a95a65fa1b75e7901cc44fe2725ccf0f8d5
kak <kak@MacBook-Pro-Ura.local>
2023-01-15 00:16:48 +0000

    RoomDetail

--- src/protected/RoomDetail/components/MainButtons.jsx
+++ src/protected/RoomDetail/components/MainButtons.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import "./MainButtons.css";
 const MainButtons = (props) => {
   return (
-    <button className={`main-buttons ${props.className || ""}`}>
+    <button className="main-buttons">
       <div className="vector">{props.mainButton || "Send Notification"}</div>
     </button>
   );
