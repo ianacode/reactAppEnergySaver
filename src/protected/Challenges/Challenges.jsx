db4506a73831f74a4b2bd4981d3a087010a56adb
kak <kak@MacBook-Pro-Ura.Home>
2023-01-22 11:40:38 +0000

    challenges links

--- src/protected/Challenges/Challenges.jsx
+++ src/protected/Challenges/Challenges.jsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import { useNavigate } from "react-router";
 import "./Challenges.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 const Challenges = () => {
+
+  const navigate = useNavigate();
+
+
   // const propsData = {
   //   btn: {
   //     label: "Back",
@@ -30,20 +35,20 @@ const Challenges = () => {
 
         <span className="not-completed-yet">Not completed yet:</span>
 
-         <div className="field-2">
+         <div className="field-2" onClick={() => navigate('/challenges/1')} >
           <div className="percentage">
             <span className="challenge-3">CHALLENGE 1</span>
           </div>
         </div>
 
-        <div className="field-2">
+        <div className="field-2" onClick={() => navigate('/challenges/2')}>
           <div className="percentage">
             <span className="challenge-3">CHALLENGE 2</span>
           </div>
         </div>
         
         
-        <div className="field-2">
+        <div className="field-2" onClick={() => navigate('/challenges/3')}>
           <div className="percentage">
             <span className="challenge-3">CHALLENGE 3</span>
           </div>
@@ -52,7 +57,7 @@ const Challenges = () => {
 
         <span className="completed">Completed:</span>
 
-        <div className="field-2">
+        <div className="field-2" onClick={() => navigate('/challenges/4')}>
           <div className="percentage-ful">
             <span className="challenge-3">CHALLENGE 4</span>
           </div>
