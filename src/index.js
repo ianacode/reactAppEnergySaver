626050d5829bba28e432be0eb6820670f0eb5754
Simone Federici <s.federici@geniusbytes.it>
2023-01-24 03:15:36 +0100

    handle /deca_22TDW/deca_22TDW_14/reactapp

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
+
+/**
+ * Turns URL path into router basename by removing everything after the last slash
+ * @param {string} path URL path, probably window.location.pathname
+ * @returns {string} final basename
+ */
+const basePath = '/deca_22TDW/deca_22TDW_14/reactapp';
+const getBasename = path => path.substr(0, path.lastIndexOf(basePath) + basePath.length);
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={getBasename(window.location.pathname)}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
